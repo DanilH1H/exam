33c2f7c7d38664d22e74d912b3c2fa29527fca10
DanilH1H <daniil.el07@yandex.ru>
2025-12-05 09:48:44 +0300

    Вопрос 16 задание 1(Задание с тестами)

---
+++ Задание 1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xunit;
+    public class Program
+{
+    static void Main()
+    {
+        string text = "Мой кот очень милый";
+    
+        Pitomets r = new Pitomets();
+        Console.WriteLine(r.aboba(text));
+    }
+
+    [Fact]
+    public void Test()
+    {
+        string text = "Мой кот очень милый";
+        Pitomets r = new Pitomets();
+        Assert.Equal("Мой пес очень милый", r.aboba(text));  
+    }
+}
+public class Pitomets
+{
+    public string aboba(string x) => x.Replace("кот", "пес");
+}
+
