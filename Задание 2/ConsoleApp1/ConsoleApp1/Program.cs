fb5071c71f2e8168a176aec190d2cc31fc073484
DanilH1H <daniil.el07@yandex.ru>
2025-12-05 09:06:30 +0300

    Вопрос 16. Задание 2. готовое задание

---
+++ Задание 2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        bool containsThree = false;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] == 3)
+            {
+                containsThree = true;
+                break;
+            }
+        }
+        Console.WriteLine(containsThree);
+    }
+}
